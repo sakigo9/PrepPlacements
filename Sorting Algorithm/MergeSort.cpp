7e70d2aa0a31a59a7b1bb5a811e89bdd81d5cbec
sakigo9 <saketprag322@gmail.com>
2020-05-10 20:22:41 +0530

    mergeSort commit

---
+++ Sorting Algorithm/MergeSort.cpp
@@ -0,0 +1,95 @@
+//example of Divide and Conquer
+//Its a complement of QuickSort
+//This	algorithm	is	used	for	sorting	a	linked	list
+//Time complexity : T(nlogn) which is much better than bubble,insertion,selection sort
+#include<iostream> 
+using namespace std;
+
+// Merges two subarrays of arr[]. 
+// First subarray is arr[l..m] 
+// Second subarray is arr[m+1..r] 
+void merge(int arr[], int l, int m, int r) 
+{ 
+	int i, j, k; 
+	int n1 = m - l + 1; 
+	int n2 = r - m; 
+
+	/* create temp arrays */
+	int L[n1], R[n2]; 
+
+	/* Copy data to temp arrays L[] and R[] */
+	for (i = 0; i < n1; i++) 
+		L[i] = arr[l + i]; 
+	for (j = 0; j < n2; j++) 
+		R[j] = arr[m + 1+ j]; 
+
+	/* Merge the temp arrays back into arr[l..r]*/
+	i = 0; // Initial index of first subarray 
+	j = 0; // Initial index of second subarray 
+	k = l; // Initial index of merged subarray 
+	while (i < n1 && j < n2) 
+	{ 
+		if (L[i] <= R[j]) 
+		{ 
+			arr[k] = L[i]; 
+			i++;            //for getting next smallest value in left array
+		} 
+		else
+		{ 
+			arr[k] = R[j]; 
+			j++; 
+		} 
+		k++;                //increasing the index of merged array having all sorted values.
+	} 
+
+// if there is any element left in the Left or Right sorted array
+
+
+	while (i < n1) 
+	{ 
+		arr[k] = L[i]; 
+		i++; 
+		k++; 
+	} 
+
+	
+	while (j < n2) 
+	{ 
+		arr[k] = R[j]; 
+		j++; 
+		k++; 
+	} 
+} 
+
+void mergeSort(int arr[], int l, int r) 
+{ 
+	if (l < r) 
+	{ 
+		
+		int m = l+(r-l)/2; 
+		mergeSort(arr, l, m); 
+		mergeSort(arr, m+1, r); 
+
+		merge(arr, l, m, r); 
+	} 
+} 
+
+int main() 
+{ 
+	int arr[100];
+	int n;
+	cout<<"Enter the number of elements in arry"<<endl;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+	
+	mergeSort(arr,0,n-1);
+	for(int i=0;i<n;i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+	
+} 
+
